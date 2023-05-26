b55cc13b8a0eb5600999d62b87fcec227d83243b
StepanNaydin <124874525+StepanNaydin@users.noreply.github.com>
2023-05-26 22:13:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,41 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+   std::ifstream file(filename);               // открываем файл на чтение
+
+    if (!file) {                                // если файл не открылся, прекращаем работу
+        std::cout << "File error!" << std::endl;
+        binary_tree tree("error");
+        return tree;
+    }
+
+    string word = "";
+
+    while (true) {
+        char ch = file.get();
+        if (ch != ' ') {
+            word += tolower(ch);
+        } else {
+            break;
+        }
+    }
+
+    binary_tree tree(word);
+    word = "";
+
+    while (!file.eof())                         // читаем содержимое файла посимвольно
+    {
+        char ch = file.get();
+        if (!isalpha(ch) && ch != ' ') {
+            ch = ' ';
+        }
+        if (ch != ' ') {
+            word += tolower(ch);
+        } else {
+            tree.insert(word);
+            word = "";
+        }
+    }
+
+    return tree;
 }
