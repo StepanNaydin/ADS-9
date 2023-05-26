c163ff668892cafb50ef6e6f2a90894c3f2ce1ad
StepanNaydin <124874525+StepanNaydin@users.noreply.github.com>
2023-05-26 22:29:05 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -4,16 +4,14 @@
 #include <iostream>
 #include <fstream>
 #include "string"
-using namespace std;
 
 class tree_elem {
  public:
-    string m_data;
+    std::string m_data;
     int count = 1;
     tree_elem* m_left;
     tree_elem* m_right;
-    tree_elem(string val)
-    {
+    tree_elem(std::string val) {
         m_left = NULL;
         m_right = NULL;
         m_data = val;
@@ -27,22 +25,19 @@ class binary_tree {
     void print_tree(tree_elem*);
 
  public:
-    binary_tree(string);
+    binary_tree(std::string);
     void print();
-    int find(string);
-    void insert(string);
+    int find(std::string);
+    void insert(std::string);
 };
 
-binary_tree::binary_tree(string key)
-{
+binary_tree::binary_tree(std::string key) {
     m_root = new tree_elem(key);
     m_size = 1;
 }
-int binary_tree::find(string key)
-{
+int binary_tree::find(std::string key) {
     tree_elem* curr = m_root;
-    while (curr && curr->m_data != key)
-    {
+    while (curr && curr->m_data != key) {
         if (curr->m_data > key){
             curr = curr->m_left;
         } else {
@@ -51,19 +46,15 @@ int binary_tree::find(string key)
     }
     return curr->count;
 }
-void binary_tree::insert(string key)
-{
+void binary_tree::insert(std::string key) {
     tree_elem* curr = m_root;
-    while (curr && curr->m_data != key)
-    {
-        if (curr->m_data > key && curr->m_left == NULL)
-        {
+    while (curr && curr->m_data != key) {
+        if (curr->m_data > key && curr->m_left == NULL) {
             curr->m_left = new tree_elem(key);
             ++m_size;
             return;
         }
-        if (curr->m_data < key && curr->m_right == NULL)
-        {
+        if (curr->m_data < key && curr->m_right == NULL) {
             curr->m_right = new tree_elem(key);
             ++m_size;
             return;
@@ -78,17 +69,14 @@ void binary_tree::insert(string key)
         curr->count++;
     }
 }
-void binary_tree::print()
-{
+void binary_tree::print() {
     print_tree(m_root);
-    cout << endl;
+    std::cout << std::endl;
 }
-void binary_tree::print_tree(tree_elem* curr)
-{
-    if (curr) // Проверка на ненулевой указатель
-    {
+void binary_tree::print_tree(tree_elem* curr) {
+    if (curr) {
         print_tree(curr->m_left);
-        cout << curr->m_data << " " << curr->count << endl;
+        std::cout << curr->m_data << " " << curr->count << std::endl;
         print_tree(curr->m_right);
     }
 }
