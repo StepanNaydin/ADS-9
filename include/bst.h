8f7f0e844602c258c4060badbda7b79aae4278ca
StepanNaydin <124874525+StepanNaydin@users.noreply.github.com>
2023-05-26 23:12:53 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -5,79 +5,63 @@
 #include <fstream>
 #include "string"
 
-class tree_elem {
- public:
-    std::string m_data;
-    int count = 1;
-    tree_elem* m_left;
-    tree_elem* m_right;
-    explicit tree_elem(std::string val) {
-        m_left = NULL;
-        m_right = NULL;
-        m_data = val;
-    }
-};
-
-class binary_tree {
+template<typename T>
+class BST {
  private:
+     struct tree_elem {
+         T m_data;
+         int count = 1;
+         tree_elem* m_left;
+         tree_elem* m_right;
+         explicit tree_elem(T val) {
+             m_left = NULL;
+             m_right = NULL;
+             m_data = val;
+         }
+     };
     tree_elem* m_root;
     int m_size;
     void print_tree(tree_elem*);
 
  public:
-    explicit binary_tree(std::string);
-    void print();
-    int find(std::string);
-    void insert(std::string);
+    explicit BST(T key) {
+        m_root = new tree_elem(key);
+        m_size = 1;
+    }
+    int find(T key) {
+        tree_elem* curr = m_root;
+        while (curr && curr->m_data != key) {
+            if (curr->m_data > key) {
+                curr = curr->m_left;
+            } else {
+                curr = curr->m_right;
+            }
+        }
+        return curr->count;
+    }
+    void insert(std::string key) {
+        tree_elem* curr = m_root;
+        while (curr && curr->m_data != key) {
+            if (curr->m_data > key && curr->m_left == NULL) {
+                curr->m_left = new tree_elem(key);
+                ++m_size;
+                return;
+            }
+            if (curr->m_data < key && curr->m_right == NULL) {
+                curr->m_right = new tree_elem(key);
+                ++m_size;
+                return;
+            }
+            if (curr->m_data > key) {
+                curr = curr->m_left;
+            } else {
+                curr = curr->m_right;
+            }
+        }
+        if (curr->m_data == key) {
+            curr->count++;
+        }
+    }
 };
 
-binary_tree::binary_tree(std::string key) {
-    m_root = new tree_elem(key);
-    m_size = 1;
-}
-int binary_tree::find(std::string key) {
-    tree_elem* curr = m_root;
-    while (curr && curr->m_data != key) {
-        if (curr->m_data > key) {
-            curr = curr->m_left;
-        } else {
-            curr = curr->m_right;
-        }
-    }
-    return curr->count;
-}
-void binary_tree::insert(std::string key) {
-    tree_elem* curr = m_root;
-    while (curr && curr->m_data != key) {
-        if (curr->m_data > key && curr->m_left == NULL) {
-            curr->m_left = new tree_elem(key);
-            ++m_size;
-            return;
-        }
-        if (curr->m_data < key && curr->m_right == NULL) {
-            curr->m_right = new tree_elem(key);
-            ++m_size;
-            return;
-        }
-        if (curr->m_data > key) {
-            curr = curr->m_left;
-        } else {
-            curr = curr->m_right;
-        }
-    }
-    if (curr->m_data == key) {
-        curr->count++;
-    }
-}
-void binary_tree::print() {
-    print_tree(m_root);
-    std::cout << std::endl;
-}
-void binary_tree::print_tree(tree_elem* curr) {
-    if (curr) {
-        print_tree(curr->m_left);
-        std::cout << curr->m_data << " " << curr->count << std::endl;
-        print_tree(curr->m_right);
-    }
-}
 #endif  // INCLUDE_BST_H_
